b1dcc26c4574c6fe391126c800af6864a1682af0
Sun Kit Tsui <sunkittsui@gmail.com>
2024-03-12 23:11:32 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <p class="py-20 text-center transition text-4xl text-green-700 hover:scale-125 hover:bg-sky-700 hover:text-white">
+      Hello tailwind!
+    </p>
+  );
+}
+
+export default App;
