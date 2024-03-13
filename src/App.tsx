aaa4a3f674ae8569e03151ab591eaaabb26870a7
Sun Kit Tsui <sunkittsui@gmail.com>
2024-03-13 16:13:24 -0500

    Implement data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import {
+  Match,
+  Suspense,
+  Switch,
+  createResource,
+  createSignal,
+} from "solid-js";
+
+const fetchProgram2 = async (programName: string) => {
+  const response = await fetch(`http://localhost:3001/${programName}.json`);
+  return response.json();
+};
+
 function App() {
+  const [programName, setProgramName] = createSignal(
+    "major_in_computer_science"
+  );
+  const [programJson] = createResource(programName, fetchProgram2);
+
   return (
-    <p class="py-20 text-center transition text-4xl text-green-700 hover:scale-125 hover:bg-sky-700 hover:text-white">
-      Hello tailwind!
-    </p>
+    <div>
+      <input
+        inputmode="text"
+        placeholder="Program Name"
+        value={programName().replaceAll("_", " ")}
+        onChange={(e) => {
+          const processedProgramName = e.target.value.replaceAll(" ", "_");
+          console.log(processedProgramName);
+          setProgramName(processedProgramName);
+        }}
+      />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Match when={programJson.error}>
+            <span>
+              Error: {`Program with name: ${programName()} is not found.`}
+            </span>
+          </Match>
+          <Match when={programJson()}>
+            <textarea class="text-sky-800 h-[90vh] w-full resize-none overflow-scroll">
+              {JSON.stringify(programJson(), null, 4)}
+            </textarea>
+          </Match>
+        </Switch>
+      </Suspense>
+    </div>
   );
 }
 
