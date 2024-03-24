1665bfcd9b9cb38813e9c930b09326ca9eb4f769
Sun Kit Tsui <sunkittsui@gmail.com>
2024-03-24 17:01:44 -0500

    feat: Use uuidv4 and title as nodeId

--- src/utils/keygen.ts
+++ src/utils/keygen.ts
@@ -1,9 +1,5 @@
-const MAX = 100000;
-const MIN = 0;
-const MAX_LEN = MAX.toString().length;
+import { v4 as uuidv4 } from "uuid";
 
-export function generateId(idBase: string) {
-  const randNum = Math.round(MIN + Math.random() * (MAX - MIN));
-  const randNumStr = randNum.toString().padStart(MAX_LEN, "0");
-  return `${idBase}-${randNumStr}`;
+export function generateId(idBase: string): string {
+  return `${idBase}-${uuidv4()}`;
 }
