373e53786a471883e4e64d1c4e87ce88c647a241
Sun Kit Tsui <sunkittsui@gmail.com>
2024-03-24 17:03:20 -0500

    feat: Add arrows to `ProgramMap`

--- src/components/NewProgramMap.tsx
+++ src/components/NewProgramMap.tsx
@@ -1,6 +1,15 @@
 import { ReactiveMap } from "@solid-primitives/map";
-import { For, JSXElement, Match, Show, Switch, onMount } from "solid-js";
+import {
+  For,
+  JSXElement,
+  Match,
+  Show,
+  Switch,
+  createSignal,
+  onMount,
+} from "solid-js";
 import { generateId } from "../utils/keygen";
+import CurvedArrow from "./utility/CurvedArrow";
 import * as T from "../types";
 
 type NodeInfo = {
@@ -23,20 +32,23 @@ function ProgramMap(props: { program: T.Program }) {
       const scrollWidth = pmContainerRef.scrollWidth;
       pmContainerRef.scrollLeft = (scrollWidth - clientWidth) / 2;
 
-      const selfWidth = pmContainerRef.clientWidth
+      const selfWidth = pmContainerRef.clientWidth;
       const childWidth = pmContainerRef.children[0].clientWidth;
 
       // Center the Program using CSS property `justify-content: center`
       // if no horizontal scrolling is required
       if (childWidth < selfWidth) {
-        pmContainerRef.style.setProperty("display", "flex")
-        pmContainerRef.style.setProperty("justify-content", "center")
+        pmContainerRef.style.setProperty("display", "flex");
+        pmContainerRef.style.setProperty("justify-content", "center");
       }
     }
-  })
+  });
 
   return (
-    <article ref={pmContainerRef} class={`h-[80vh] w-[90vw] overflow-scroll rounded-lg border-2 border-solid border-black bg-yellow-50 p-5`}>
+    <article
+      ref={pmContainerRef}
+      class={`relative h-[80vh] w-[90vw] overflow-scroll rounded-lg border-2 border-solid border-black bg-yellow-50 p-5`}
+    >
       <Program program={props.program} />
     </article>
   );
@@ -44,6 +56,8 @@ function ProgramMap(props: { program: T.Program }) {
 
 function Program(props: { program: T.Program }) {
   console.log("Program", props.program);
+
+  const id = generateId(props.program.title);
 
   const contents = (
     <div class="flex flex-col items-center justify-center">
@@ -59,38 +73,47 @@ function Program(props: { program: T.Program }) {
   );
 
   return (
-      <Node id={generateId(props.program.title)} nodeContent={contents}>
-        <Switch fallback={<FallbackMessage target="Program" />}>
-          <Match when={props.program.requirements}>
-            {(reqs) => <Requirements reqs={reqs()} />}
-          </Match>
-          <Match when={props.program.requirements === null}>
-            <div>No Requirements Listed</div>
-          </Match>
-        </Switch>
-      </Node>
+    <Node id={id} nodeContent={contents}>
+      <Switch fallback={<FallbackMessage target="Program" />}>
+        <Match when={props.program.requirements}>
+          {(reqs) => <Requirements reqs={reqs()} parentId={id} />}
+        </Match>
+        <Match when={props.program.requirements === null}>
+          <div>No Requirements Listed</div>
+        </Match>
+      </Switch>
+    </Node>
   );
 }
 
-function Requirements(props: { reqs: T.Requirements }) {
+function Requirements(props: { reqs: T.Requirements; parentId: string }) {
   console.log("Requirements", props.reqs);
   return (
     <Switch>
       <Match when={props.reqs.type === T.RequirementsType.Single}>
         <RequirementModule
           reqModule={(props.reqs as T.SingleRequirement).data}
+          parentId={props.parentId}
         />
       </Match>
       <Match when={props.reqs.type === T.RequirementsType.Many}>
         <For each={(props.reqs as T.ManyRequirements).data}>
-          {(reqModule) => <RequirementModule reqModule={reqModule} />}
+          {(reqModule) => (
+            <RequirementModule
+              reqModule={reqModule}
+              parentId={props.parentId}
+            />
+          )}
         </For>
       </Match>
     </Switch>
   );
 }
 
-function RequirementModule(props: { reqModule: T.RequirementModule }) {
+function RequirementModule(props: {
+  reqModule: T.RequirementModule;
+  parentId: string;
+}) {
   console.log("RequirementModule", props.reqModule);
   return (
     <Switch>
@@ -102,6 +125,7 @@ function RequirementModule(props: { reqModule: T.RequirementModule }) {
       >
         <SingleBasicRequirement
           req={props.reqModule as T.SingleBasicRequirement}
+          parentId={props.parentId}
         />
       </Match>
       <Match
@@ -109,73 +133,83 @@ function RequirementModule(props: { reqModule: T.RequirementModule }) {
           props.reqModule.type === T.RequirementModuleType.BasicRequirements
         }
       >
-        <BasicRequirements req={props.reqModule as T.BasicRequirements} />
+        <BasicRequirements
+          req={props.reqModule as T.BasicRequirements}
+          parentId={props.parentId}
+        />
       </Match>
       <Match
         when={
           props.reqModule.type === T.RequirementModuleType.SelectOneEmphasis
         }
       >
-        <Unimplemented rawContent={props.reqModule} />
+        <Unimplemented rawContent={props.reqModule} parentId={props.parentId} />
       </Match>
       <Match when={props.reqModule.type === T.RequirementModuleType.Label}>
-        <ModuleLabel req={props.reqModule as T.ModuleLabel} />
+        <ModuleLabel
+          req={props.reqModule as T.ModuleLabel}
+          parentId={props.parentId}
+        />
       </Match>
       <Match
         when={props.reqModule.type === T.RequirementModuleType.Unimplemented}
       >
-        <Unimplemented rawContent={props.reqModule} />
+        <Unimplemented rawContent={props.reqModule} parentId={props.parentId} />
       </Match>
     </Switch>
   );
 }
 
-function SingleBasicRequirement(props: { req: T.SingleBasicRequirement }) {
+function SingleBasicRequirement(props: {
+  req: T.SingleBasicRequirement;
+  parentId: string;
+}) {
+  const id = generateId(props.req.data.title || "SingleBasicRequirement");
   const content = <h3 class="w-[80%] text-center">{props.req.data.title}</h3>;
 
   return (
-    <Node
-      id={generateId(props.req.data.title || "SingleBasicRequirement")}
-      nodeContent={content}
-    >
-      <Requirement req={props.req.data.requirement} />
+    <Node id={id} parentId={props.parentId} nodeContent={content}>
+      <Requirement req={props.req.data.requirement} parentId={id} />
     </Node>
   );
 }
 
-function BasicRequirements(props: { req: T.BasicRequirements }) {
+function BasicRequirements(props: {
+  req: T.BasicRequirements;
+  parentId: string;
+}) {
   console.log("BasicRequirements", props.req);
   console.log("BasicRequirements title", props.req.data.title);
+  const id = generateId(props.req.data.title || "SingleBasicRequirement");
   const content = <h3 class="w-[80%] text-center">{props.req.data.title}</h3>;
 
   return (
-    <Node
-      id={generateId(props.req.data.title || "SingleBasicRequirement")}
-      nodeContent={content}
-    >
+    <Node id={id} parentId={props.parentId} nodeContent={content}>
       <div class="flex flex-row items-start justify-center">
         <For each={props.req.data.requirements}>
-          {(req) => <Requirement req={req} />}
+          {(req) => <Requirement req={req} parentId={id} />}
         </For>
       </div>
     </Node>
   );
 }
 
-function ModuleLabel(props: { req: T.ModuleLabel }) {
+function ModuleLabel(props: { req: T.ModuleLabel; parentId: string }) {
   return (
     <Node
       id={generateId(props.req.data.title)}
+      parentId={props.parentId}
       nodeContent={<h3 class="w-[80%] text-center">{props.req.data.title}</h3>}
     />
   );
 }
 
-function Unimplemented(props: { rawContent: unknown }) {
+function Unimplemented(props: { rawContent: unknown; parentId: string }) {
   const id = generateId("Unimplemented");
   return (
     <Node
       id={id}
+      parentId={props.parentId}
       nodeContent={
         <div class="flex flex-col items-center justify-center p-5">
           <h3 class="w-[80%] text-center">Unimplemented</h3>
@@ -194,18 +228,27 @@ function Unimplemented(props: { rawContent: unknown }) {
   );
 }
 
-function Requirement(props: { req: T.Requirement }) {
+function Requirement(props: { req: T.Requirement; parentId: string }) {
   console.log("Requirement", props.req);
   return (
     <Switch>
       <Match when={props.req.type === T.RequirementType.Courses}>
-        <Courses data={(props.req as T.Courses).data} />
+        <Courses
+          data={(props.req as T.Courses).data}
+          parentId={props.parentId}
+        />
       </Match>
       <Match when={props.req.type === T.RequirementType.SelectFromCourses}>
-        <SelectFromCourses data={(props.req as T.SelectFromCourses).data} />
+        <SelectFromCourses
+          data={(props.req as T.SelectFromCourses).data}
+          parentId={props.parentId}
+        />
       </Match>
       <Match when={props.req.type === T.RequirementType.Label}>
-        <RequirementLabel data={(props.req as T.RequirementLabel).data} />
+        <RequirementLabel
+          data={(props.req as T.RequirementLabel).data}
+          parentId={props.parentId}
+        />
       </Match>
     </Switch>
   );
@@ -213,16 +256,18 @@ function Requirement(props: { req: T.Requirement }) {
 
 function Courses(props: {
   data: { title: string | null; courses: T.CourseEntry[] };
+  parentId: string;
 }) {
+  const id = generateId(props.data.title || "Courses");
   const content = (
     <h3 class="w-[80%] text-center">{props.data.title || "Courses"}</h3>
   );
 
   return (
-    <Node id={generateId(props.data.title || "Courses")} nodeContent={content}>
+    <Node id={id} nodeContent={content} parentId={props.parentId}>
       <div class="flex flex-col items-center justify-center gap-20">
         <For each={props.data.courses}>
-          {(entry) => <CourseEntry entry={entry} />}
+          {(entry) => <CourseEntry entry={entry} parentId={id} />}
         </For>
       </div>
     </Node>
@@ -231,16 +276,22 @@ function Courses(props: {
 
 function SelectFromCourses(props: {
   data: { title: string; courses: T.CourseEntry[] | null };
+  parentId: string;
 }) {
+  const id = generateId(props.data.title);
+
   return (
     <Node
-      id={generateId(props.data.title)}
+      id={id}
+      parentId={props.parentId}
       nodeContent={
         <div class="flex flex-col items-center justify-center gap-5">
           <h3 class="w-[80%] text-center">{props.data.title}</h3>
           <Show when={props.data.courses} fallback={<p>No courses listed.</p>}>
             {(entries) =>
-              entries().map((entry) => <CourseEntry entry={entry} />)
+              entries().map((entry) => (
+                <CourseEntry entry={entry} parentId={id} />
+              ))
             }
           </Show>
         </div>
@@ -251,10 +302,14 @@ function SelectFromCourses(props: {
 
 function RequirementLabel(props: {
   data: { title: string | null; reqNarrative: string | null };
+  parentId: string;
 }) {
+  const id = generateId(props.data.title || "RequirementLabel");
+
   return (
     <Node
-      id={generateId(props.data.title || "RequirementLabel")}
+      id={id}
+      parentId={props.parentId}
       nodeContent={
         <>
           <h3 class="w-[80%] text-center">{props.data.title}</h3>
@@ -265,29 +320,50 @@ function RequirementLabel(props: {
   );
 }
 
-function CourseEntry(props: { entry: T.CourseEntry, isNested?: boolean }) {
+function CourseEntry(props: {
+  entry: T.CourseEntry;
+  isNested?: boolean;
+  parentId: string;
+}) {
   return (
     <Switch>
       <Match when={props.entry.type === T.CourseEntryType.And}>
-        <And entries={(props.entry as T.And).data} isNested={props.isNested} />
+        <And
+          entries={(props.entry as T.And).data}
+          isNested={props.isNested}
+          parentId={props.parentId}
+        />
       </Match>
       <Match when={props.entry.type === T.CourseEntryType.Or}>
-        <Or entries={(props.entry as T.Or).data} />
+        <Or entries={(props.entry as T.Or).data} parentId={props.parentId} />
       </Match>
       <Match when={props.entry.type === T.CourseEntryType.Course}>
-        <Course course={(props.entry as T.EntryCourse).data} />
+        <Course
+          course={(props.entry as T.EntryCourse).data}
+          parentId={props.parentId}
+        />
       </Match>
       <Match when={props.entry.type === T.CourseEntryType.Label}>
-        <Label label={(props.entry as T.EntryLabel).data} />
+        <Label
+          label={(props.entry as T.EntryLabel).data}
+          parentId={props.parentId}
+        />
       </Match>
     </Switch>
   );
 }
 
-function And(props: { entries: T.CourseEntry[], isNested?: boolean }) {
+function And(props: {
+  entries: T.CourseEntry[];
+  isNested?: boolean;
+  parentId: string;
+}) {
+  const id = generateId("And");
+
   return (
     <Node
-      id={generateId("And")}
+      id={id}
+      parentId={props.parentId}
       nodeContent={
         <div class="flex flex-col items-center justify-center gap-5 p-5">
           <h3 class="w-[80%] text-center">And</h3>
@@ -295,7 +371,9 @@ function And(props: { entries: T.CourseEntry[], isNested?: boolean }) {
             class={`flex ${props.entries.length <= 3 && props.isNested === false ? "flex-row" : "flex-col"} items-center justify-center gap-5`}
           >
             <For each={props.entries}>
-              {(entry) => <CourseEntry entry={entry} isNested={true} />}
+              {(entry) => (
+                <CourseEntry entry={entry} isNested={true} parentId={id} />
+              )}
             </For>
           </div>
         </div>
@@ -305,22 +383,29 @@ function And(props: { entries: T.CourseEntry[], isNested?: boolean }) {
 }
 
 // TODO: Display different options as horizontal branches of the program map
-function Or(props: { entries: T.CourseEntry[] }) {
+function Or(props: { entries: T.CourseEntry[]; parentId: string }) {
   console.log("Or entry length: ", props.entries.length);
 
   return (
     <div class="flex flex-row gap-5">
-    <For each={props.entries}>
-      {(entry) => <CourseEntry entry={entry} isNested={true} />}
-    </For>
+      <For each={props.entries}>
+        {(entry) => (
+          <CourseEntry
+            entry={entry}
+            isNested={true}
+            parentId={props.parentId}
+          />
+        )}
+      </For>
     </div>
-  )
+  );
 }
 
-function Label(props: { label: T.Label }) {
+function Label(props: { label: T.Label; parentId: string}) {
   return (
     <Node
       id={generateId(props.label.name)}
+      parentId={props.parentId}
       nodeContent={
         <>
           <h3 class="w-[80%] text-center">{props.label.name}</h3>
@@ -330,10 +415,12 @@ function Label(props: { label: T.Label }) {
   );
 }
 
-function Course(props: { course: T.Course }) {
+function Course(props: { course: T.Course; parentId: string }) {
+  const id = generateId(props.course.name || "Course");
   return (
     <Node
-      id={generateId(props.course.name || "Course")}
+      id={id}
+      parentId={props.parentId}
       nodeContent={
         <div class="flex flex-col items-center justify-center">
           <h3 class="w-[80%] text-center">
@@ -355,20 +442,74 @@ function Course(props: { course: T.Course }) {
 function Node(props: {
   id: string;
   nodeContent: JSXElement;
+  parentId?: string;
   children?: JSXElement;
 }) {
+  const [arrow, setArrow] = createSignal<JSXElement | undefined>(undefined);
+
+  let nodeRef: HTMLDivElement | undefined;
+  let svgRef: SVGSVGElement | undefined;
+
+  onMount(() => {
+    if (!props.parentId) return;
+
+    const parentNode = document.getElementById(props.parentId);
+    const parentBoundingRect = parentNode?.getBoundingClientRect();
+    const selfBoundingRect = nodeRef?.getBoundingClientRect();
+    const svgBoundingRect = svgRef?.getBoundingClientRect();
+
+    console.log("parentBoundingRect", parentBoundingRect);
+    console.log("selfBoundingRect", selfBoundingRect);
+    console.log("svgBoundingRect", svgBoundingRect);
+
+    if (selfBoundingRect && parentBoundingRect && svgBoundingRect) {
+      const { x, y, height, width } = selfBoundingRect;
+      console.log({ x, y, height, width });
+
+      const {
+        x: parentX,
+        y: parentY,
+        height: parentHeight,
+        width: parentWidth,
+      } = parentBoundingRect;
+      console.log({ parentX, parentY, parentHeight, parentWidth });
+
+      const { x: offsetX, y: offsetY } = svgBoundingRect;
+      console.log({ offsetX, offsetY });
+
+      const startX = parentX + parentWidth / 2 - offsetX;
+      const startY = parentY + parentHeight - offsetY;
+
+      const endX = x + width / 2 - offsetX;
+      const endY = y - offsetY;
+
+      const arrow = (
+        <CurvedArrow id="" x1={startX} y1={startY} x2={endX} y2={endY} />
+      );
+
+      console.log("Hello from onMount");
+      console.log(props.parentId);
+
+      setArrow(arrow);
+    }
+  });
+
   return (
-    <div class="node-container flex flex-shrink-0 w-max flex-col gap-20 p-10 border-black border rounded">
-      <div class="section-container flex flex-row justify-center items-center">
+    <div class="node-container flex w-max flex-shrink-0 flex-col gap-20 rounded border border-black p-10">
+      <div class="section-container flex flex-row items-center justify-center">
         <section
           id={props.id}
-          class="flex min-h-[120px] min-w-[250px] items-center justify-center rounded-lg border-2 border-solid border-black bg-sky-100 hover:bg-sky-300"
+          ref={nodeRef}
+          class="flex min-h-[120px] min-w-[250px] items-center justify-center rounded-lg border-2 border-solid border-black bg-sky-100 transition hover:bg-sky-300"
         >
           {props.nodeContent}
         </section>
       </div>
+      <svg class="absolute h-0 w-0 overflow-visible" ref={svgRef}>
+        {arrow()}
+      </svg>
       {props.children ? (
-        <div class="children-container flex flex-shrink-0 flex-row justify-center items-start gap-20">
+        <div class="children-container flex flex-shrink-0 flex-row items-start justify-center gap-20">
           {props.children}
         </div>
       ) : null}
---
+++ src/components/utility/CurvedArrow.tsx
@@ -0,0 +1,87 @@
+import { JSXElement } from "solid-js";
+
+function CurvedArrow(props: {
+  id: string;
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  strokeWidth?: number;
+  showCurveLines?: boolean;
+}): JSXElement {
+  const mid = {
+    x: (props.x1 + props.x2) / 2,
+    y: (props.y1 + props.y2) / 2,
+  };
+
+  const controlPoint1 = {
+    x: props.x1,
+    y: mid.y,
+  };
+
+  const controlPoint2 = {
+    x: (mid.x + props.x2) / 2,
+    y: props.y2,
+  };
+
+  const directives = `M ${props.x1} ${props.y1} Q ${controlPoint1.x} ${controlPoint1.y}, ${mid.x} ${mid.y} T ${props.x2} ${props.y2}`;
+
+  const curveDemoLines = (
+    <>
+      <line
+        x1={props.x1}
+        y1={props.y1}
+        x2={controlPoint1.x}
+        y2={controlPoint1.y}
+        stroke="blue"
+        stroke-dasharray="5,5"
+      />
+      <circle cx={controlPoint1.x} cy={controlPoint1.y} r="2" fill="blue" />
+      <line
+        x1={controlPoint1.x}
+        y1={controlPoint1.y}
+        x2={mid.x}
+        y2={mid.y}
+        stroke="blue"
+        stroke-dasharray="5,5"
+      />
+      <line
+        x1={mid.x}
+        y1={mid.y}
+        x2={controlPoint2.x}
+        y2={controlPoint2.y}
+        stroke="blue"
+        stroke-dasharray="5,5"
+      />
+      <circle cx={controlPoint2.x} cy={controlPoint2.y} r="2" fill="blue" />
+      <line
+        x1={controlPoint2.x}
+        y1={controlPoint2.y}
+        x2={props.x2}
+        y2={props.y2}
+        stroke="blue"
+        stroke-dasharray="5,5"
+      />
+    </>
+  );
+
+  return (
+    <>
+      {props.showCurveLines === true ? curveDemoLines : null}
+      <path
+        id={props.id}
+        style={{
+          transition: "0.25s",
+        }}
+        class="normal-arrow"
+        d={directives}
+        fill="none"
+        stroke="black"
+        stroke-width={props.strokeWidth || 3}
+      />
+      ;
+    </>
+  );
+}
+
+export default CurvedArrow;
